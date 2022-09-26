299431ef260f2497a51f51e036999312eb5b24cf
Lakhan Gupta <99022559+lakhangupta1@users.noreply.github.com>
2022-09-26 18:56:38 +0530

    Create Rotate(rghtangle).cpp

---
+++ Rotate(rghtangle).cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    } cout<<endl;
+
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(a[i][j],a[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            swap(a[i][j],a[i][n-1-j]);
+        }
+    }
+
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    
+
+}
